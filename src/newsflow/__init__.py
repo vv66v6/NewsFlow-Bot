ec3bb8b6871bfe2a42badc271ab4215b264d1f84
Lynthar <6722766+Lynthar@users.noreply.github.com>
2026-07-17 17:41:30 -0700

    release: version 0.9.2

--- src/newsflow/__init__.py
+++ src/newsflow/__init__.py
@@ -5,5 +5,5 @@ A flexible RSS feed aggregator that pushes news to Discord and Telegram
 with optional translation support.
 """
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 __author__ = "NewsFlow Contributors"
